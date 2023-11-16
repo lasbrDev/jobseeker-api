39ea306201ce5a57398e770c928f84a499f54cb0
Luciano Alves <lasbr_alves@outlook.com>
2023-11-15 21:42:29 -0300

    Project started

---
+++ src/main/java/br/com/jobseekers/jobapi/JobapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.jobseekers.jobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobapiApplication.class, args);
+	}
+}
