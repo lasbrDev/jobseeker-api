baefd78e68b67af3784ae9f44754f5057542e71f
Luciano Alves <lasbr_alves@outlook.com>
2023-11-21 19:52:26 -0300

    feat: added post method in ReviewController also added impl in ReviewService and ReviewServiceImpl

--- src/main/java/br/com/jobseekers/jobapi/review/ReviewController.java
+++ src/main/java/br/com/jobseekers/jobapi/review/ReviewController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/companiew/{companyId}")
+@RequestMapping("/companies/{companyId}")
 public class ReviewController {
     
     private ReviewService reviewService;
@@ -22,5 +24,16 @@ public class ReviewController {
     public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId) {
         List<Review> reviews = reviewService.getAllReviews(companyId);
         return ResponseEntity.ok(reviews);
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<Review> addReview(@PathVariable Long companyId, @RequestBody Review review) {
+
+        boolean isRiviewAdded = reviewService.addReview(companyId, review);
+        if (!isRiviewAdded) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            return ResponseEntity.ok(review);
+        }
     }
 }
--- src/main/java/br/com/jobseekers/jobapi/review/impl/ReviewServiceImpl.java
+++ src/main/java/br/com/jobseekers/jobapi/review/impl/ReviewServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import br.com.jobseekers.jobapi.company.Company;
+import br.com.jobseekers.jobapi.company.CompanyService;
 import br.com.jobseekers.jobapi.review.Review;
 import br.com.jobseekers.jobapi.review.ReviewRepository;
 import br.com.jobseekers.jobapi.review.ReviewService;
@@ -11,15 +13,28 @@ import br.com.jobseekers.jobapi.review.ReviewService;
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
+    private final CompanyService companyService;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findAllByCompanyId(companyId);
         return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if (company != null) {
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }
+        return false;
     }
 }
