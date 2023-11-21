b9f7286e8388d4353e559a7a6213289df8fd603a
Luciano Alves <lasbr_alves@outlook.com>
2023-11-21 11:34:33 -0300

    feat: added ReviewService and ReviewServiceImpl

---
+++ src/main/java/br/com/jobseekers/jobapi/review/ReviewService.java
@@ -0,0 +1,5 @@
+package br.com.jobseekers.jobapi.review;
+
+public interface ReviewService {
+    
+}
---
+++ src/main/java/br/com/jobseekers/jobapi/review/impl/ReviewServiceImpl.java
@@ -0,0 +1,6 @@
+package br.com.jobseekers.jobapi.review.impl;
+
+import br.com.jobseekers.jobapi.review.ReviewService;
+
+public class ReviewServiceImpl implements ReviewService {
+}
